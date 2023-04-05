9ae02d2d2f67651d846da69231cfc750283ca575
IVO SINGER <47070407@ort>
2023-04-05 11:46:18 -0300

    first commit

--- Script.js
+++ Script.js
@@ -1,8 +1,55 @@
-let arr = ["hacer lo de lengua", "dormir", "hacer la sopa"];
-arr.map((x)=>{
-    var Contenido = document.createElement("li");
-    Contenido.innerHTML = x;
-    document.getElementById("ESCRIBIR").appendChild(Contenido);
-})
+let arr = [];
 
+function Agregar(e) {
+  e.preventDefault();
+
+  var Tarea = document.getElementById("Tarea").value;
+  if (document.getElementById("Tarea").value == "") {
+    alert("No puede haber campos vacios :(");
+  } else {
+    arr.push({ texto: Tarea, completada: false, fecha: new Date() });
+    document.getElementById("ESCRIBIR").innerHTML = "";
+    arr.map((x, index) => {
+      var Contenido = document.createElement("li");
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("checkbox");
+      checkbox.onclick = function () {
+        tacharTarea(index);
+      };
+      Contenido.appendChild(checkbox);
+      if (x.completada) {
+        Contenido.innerHTML += "<del>" + x.texto + "</del>";
+        checkbox.checked = true;
+      } else {
+        Contenido.innerHTML += x.texto;
+        checkbox.checked = false;
+      }
+      document.getElementById("ESCRIBIR").appendChild(Contenido);
+    });
+  }
+}
+
+function tacharTarea(tarea) {
+    tarea.completada = !tarea.completada;
+    document.getElementById("ESCRIBIR").innerHTML = "";
+    arr.map((x, index)=>{
+        var Contenido = document.createElement("li");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        checkbox.onclick = function() {
+            tacharTarea(x);
+        }
+        Contenido.appendChild(checkbox);
+        if (x.completada) {
+            Contenido.innerHTML += '<del>' + x.texto + '</del>';
+            checkbox.checked = true;
+        } else {
+            Contenido.innerHTML += x.texto;
+            checkbox.checked = false;
+        }
+        document.getElementById("ESCRIBIR").appendChild(Contenido);
+    });
+}
 
