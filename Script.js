a383b4147c71e5907749bd980b2d0fc2c7cb4b0a
IVO SINGER <47070407@ort>
2023-04-05 12:11:35 -0300

    aa

--- Script.js
+++ Script.js
@@ -1,55 +1,51 @@
+
+
 let arr = [];
 
-function Agregar(e) {
-  e.preventDefault();
+function Agregar(e){
+    e.preventDefault();
+    
+    var Tarea = document.getElementById("Tarea").value;
+     if (document.getElementById("Tarea").value == "") 
+    {
+        alert("No puede haber campos vacios :(");
+    }
+    else 
+    {
+   arr.push(Tarea);
+   document.getElementById("ESCRIBIR").innerHTML = "";
+   arr.map((x)=>{
+    var Contenido = document.createElement("li");
+    var checkbox = document.createElement("input")
+        checkbox.type="checkbox"
+    Contenido.appendChild(checkbox)
+    checkbox.classList.add("checkbox")
+    Contenido.innerHTML += x;
+    document.getElementById("ESCRIBIR").appendChild(Contenido);
+    checkbox.onclick= function()
+    {}
+})
+}
+}
 
-  var Tarea = document.getElementById("Tarea").value;
-  if (document.getElementById("Tarea").value == "") {
-    alert("No puede haber campos vacios :(");
-  } else {
-    arr.push({ texto: Tarea, completada: false, fecha: new Date() });
-    document.getElementById("ESCRIBIR").innerHTML = "";
-    arr.map((x, index) => {
-      var Contenido = document.createElement("li");
-      var checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.classList.add("checkbox");
-      checkbox.onclick = function () {
-        tacharTarea(index);
-      };
-      Contenido.appendChild(checkbox);
-      if (x.completada) {
+function tachar (e)
+{
+var checkbox= document.getElementsByClassName("checkbox")
+for(i=0; i< checkbox.length; i++)
+{
+    if(onclick[1].checked)
+    {
         Contenido.innerHTML += "<del>" + x.texto + "</del>";
         checkbox.checked = true;
-      } else {
+    }
+    else
+    {
         Contenido.innerHTML += x.texto;
         checkbox.checked = false;
-      }
-      document.getElementById("ESCRIBIR").appendChild(Contenido);
-    });
-  }
+    }
 }
 
-function tacharTarea(tarea) {
-    tarea.completada = !tarea.completada;
-    document.getElementById("ESCRIBIR").innerHTML = "";
-    arr.map((x, index)=>{
-        var Contenido = document.createElement("li");
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.classList.add("checkbox");
-        checkbox.onclick = function() {
-            tacharTarea(x);
-        }
-        Contenido.appendChild(checkbox);
-        if (x.completada) {
-            Contenido.innerHTML += '<del>' + x.texto + '</del>';
-            checkbox.checked = true;
-        } else {
-            Contenido.innerHTML += x.texto;
-            checkbox.checked = false;
-        }
-        document.getElementById("ESCRIBIR").appendChild(Contenido);
-    });
+
 }
+
 
